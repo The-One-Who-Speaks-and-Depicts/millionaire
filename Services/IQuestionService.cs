c8b5dd534d9eda9e53318d0212aa3e0552f04d2a
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-09-02 16:25:20 +0400

    ADD: IQuestionService

---
+++ Services/IQuestionService.cs
@@ -0,0 +1,10 @@
+using millionaire.Models;
+using System.Collections.Generic;
+
+namespace millionaire.Services
+{
+    public interface IQuestionService
+    {
+        List<Question> GetAllQuestions();
+    }
+}
