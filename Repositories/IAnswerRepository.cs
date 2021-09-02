2f58c4a0f1675004e039db66b209346037602db2
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-09-02 16:04:21 +0400

    DELETE: IAnswerRepository deprecated

--- Repositories/IAnswerRepository.cs
+++
@@ -1,9 +0,0 @@
-using millionaire.Models;
-using System.Collections.Generic;
-namespace millionaire.Repos
-{
-    public interface IAnswerRepository
-    {
-        List<Answer> GetAllAnswers();
-    }
-}
