aaaf7a2cf631e5ce5545b25e35e05d3e8737d882
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-09-02 16:33:18 +0400

    ADD: SQLiteQuestionService

---
+++ Services/SQLiteQuestionService.cs
@@ -0,0 +1,19 @@
+using millionaire.Models;
+using millionaire.Repos;
+using System.Collections.Generic;
+
+namespace millionaire.Services
+{
+    public class SQLiteQuestionService : IQuestionService
+    {
+        private readonly IQuestionRepository _questionRepo;
+
+        public SQLiteQuestionService(IQuestionRepository questionRepo)
+        {
+            _questionRepo = questionRepo;
+        }
+
+        public List<Question> GetAllQuestions() => _questionRepo.GetAllQuestions();
+
+    }
+}
